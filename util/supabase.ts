b58dfc97c81ed969b7de19f98466af07d600c244
dondycles <johnrod.dondoyano@gmail.com>
2023-09-02 15:00:23 +0800

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   util/supabase.ts

--- util/supabase.ts
+++ util/supabase.ts
@@ -1,9 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
+const supabseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY || "";
 
-export const supabaseAdmin = createClient(
-  "https://sbpgmjqfpifglrydzkpq.supabase.co",
-  supabaseKey as string
-);
+export const supabaseAdmin = createClient(supabseUrl, supabaseKey);
